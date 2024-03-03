3aeae2d68ef23090c9735630fd97002494eda198
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-03-03 07:48:29 +0530

    add body section

--- google_search_engine/src/app/page.js
+++ google_search_engine/src/app/page.js
@@ -1,4 +1,6 @@
 import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
 import React from 'react'
 
 function page() {
@@ -6,6 +8,17 @@ function page() {
     <>
     
     <HomeHeader />
+    <div className='flex flex-col items-center mt-24'>
+      <Image src="https://imgs.search.brave.com/vTCwcACTcg2o4L_XU9oOIfkA6VLpBo1CJ618ShAOhSI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE2LzEx/L05ldy1Hb29nbGUt/TG9nby00OTd4NTAw/LmpwZw"
+      alt="google logo"
+      height={80}
+      width={200}
+      priority
+      style={{width: "auto"}}
+      />
+      
+      <HomeSearch />
+    </div>
 
     </>
   )
